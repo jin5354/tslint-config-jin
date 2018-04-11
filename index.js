de6c805e22af06e39619b7fd869b2f340bbec564
jin5354 <xiaoyanjinx@gmail.com>
2018-04-11 20:24:31 +0800

    [feat] update config

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ['tslint-config-standard'],
   rules: {
     'space-before-function-paren': false,
-    whitespace: false
+    'whitespace': [true, 'check-operator']
   }
 }
